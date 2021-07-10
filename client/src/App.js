ac43e5869b7e290cae2af17be603e11a437a23a9
jmurphy5613 <jmurphy5613@gmail.com>
2021-07-10 14:39:59 -0400

    finished desktop view design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="two-column-div">
+        <div className="left-column">
+          <h1>Get insights that help your buissness grow</h1>
+          <p>
+          </p>
+        </div>
+        <div className="right-column">
 
+        </div>
+      </div>
     </div>
   );
 }
